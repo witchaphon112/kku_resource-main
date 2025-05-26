ff3e5b85f2dda836c40764bc7f4d6ea11f1d1e04
Witchaphon103 <64647643+Witchaphon103@users.noreply.github.com>
2025-05-26 22:54:52 +0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './app.css'; 
 import App from './App.tsx'
+import Modal from 'react-modal';
 
 // Import PrimeReact styles
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import 'primeflex/primeflex.css'
+
+// Set app element for React Modal
+Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
