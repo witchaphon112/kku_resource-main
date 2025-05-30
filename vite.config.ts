06945a4a5007e16cf6253a36c069428726b03ce4
Witchayy <>
2025-05-30 11:25:43 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/kku_resource-main/',
+  base: process.env.NODE_ENV === 'production' ? '/kku_resource-main/' : '/',
   plugins: [react()],
 });
