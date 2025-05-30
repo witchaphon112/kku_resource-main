279e85eda4426560bba1f4f91495cd7690d9214d
Witchayy <>
2025-05-30 11:18:16 +0700

    update file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import './app.css'; 
+import './App.css'; 
 import App from './App.tsx'
 import Modal from 'react-modal';
 
